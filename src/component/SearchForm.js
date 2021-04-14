d8be6602dcd9a401fb262f7a86278bfed22d6549
Yulia Bandura <yuliabandura@gmail.com>
2021-04-14 17:28:10 +0300

    add input

---
+++ src/component/SearchForm.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.currentTarget.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.query);
+    this.props.onSubmit(this.state.query);
+
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          value={this.state.query}
+          onChange={this.handleChange}
+          type="text"
+          // autocomplete="off"
+          // autofocus
+          placeholder="Search movies"
+        />
+
+        <button type="submit">
+          <span>Search</span>
+        </button>
+      </form>
+    );
+  }
+}
+
+export default SearchForm;
--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import SearchForm from '../component/SearchForm';
+import Axios from 'axios';
 
 class MoviesPage extends Component {
-  state = {};
+  state = { movies: [] };
+
+  onChangeQuery = query => {
+    const apiKey = 'bfc0b177c45bde411d6d53ddc48eee25';
+    Axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=en-US&page=1&include_adult=false`,
+    ).then(response => {
+      this.setState({ movies: response.data.results });
+    });
+  };
 
   render() {
-    return <h1>Movie</h1>;
+    const { movies } = this.state;
+    return (
+      <>
+        <h1>Movie</h1>
+        <SearchForm onSubmit={this.onChangeQuery} />
+        <ul>
+          {movies.map(({ title, id }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`}>{title}</Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
   }
 }
 export default MoviesPage;
