ed9a17e3cc6208cee60deed8812475c3920f8a27
Yulia Bandura <yuliabandura@gmail.com>
2021-04-14 16:52:04 +0300

    add component

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -1,9 +1,13 @@
 import { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
 import Axios from 'axios';
+
+import Cast from '../component/Cast';
+import Reviews from '../component/Reviews';
 
 class MovieDetailsPage extends Component {
   state = {
-    poster_path: null,
+    poster_path: '',
     title: null,
     name: null,
     release_date: null,
@@ -17,9 +21,10 @@ class MovieDetailsPage extends Component {
     const response = await Axios.get(
       `https://api.themoviedb.org/3/movie/${movieId}?api_key=bfc0b177c45bde411d6d53ddc48eee25&language=en-US`,
     );
-    // console.log(response.data);
+    console.log(response.data);
 
     this.setState({ ...response.data });
+    this.setState({ release_date: this.state.release_date.slice(0, 4) });
     console.log(this.state.movie);
   }
 
@@ -34,6 +39,8 @@ class MovieDetailsPage extends Component {
     } = this.state;
 
     const scope = vote_average * 10;
+    const baseURL = 'https://image.tmdb.org/t/p/w300';
+    const imgURL = baseURL + poster_path;
     return (
       <>
         <h1>
@@ -48,10 +55,20 @@ class MovieDetailsPage extends Component {
             <li key={name}>{name}</li>
           ))}
         </ul>
-        <img
-          src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-          alt={title}
-        />
+        <img src={imgURL} alt={title} />
+
+        <h2>Additional information</h2>
+        <ul>
+          <li>
+            <NavLink to={`${this.props.match.url}/cast`}>Cast</NavLink>
+          </li>
+          <li>
+            <NavLink to={`${this.props.match.url}/reviews`}>Reviews</NavLink>
+          </li>
+        </ul>
+
+        <Route path={`${this.props.match.path}/cast`} component={Cast} />
+        <Route path={`${this.props.match.path}/reviews`} component={Reviews} />
       </>
     );
   }
